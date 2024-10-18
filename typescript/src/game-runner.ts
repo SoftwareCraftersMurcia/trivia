1e2186569c092776175122d63292afa94f55784a
Juan Sánchez <jsmontesinos@gmail.com>
2024-10-18 19:10:49 +0200

    Fix Change correct / wrong percentage

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -11,7 +11,7 @@ export class GameRunner {
         Array.from({length: 100}).forEach((_, index) => {
             game.roll(index % 6 + 1);
         
-            if (index % 2 == 0) {
+            if (index % 10 == 0) {
             notAWinner = game.wrongAnswer();
             } else {
             notAWinner = game.wasCorrectlyAnswered();
