f06ce13ea269cb1d4227c28227e8c38eaf5ffd4b
jorge-ramirez <jorge.ramirez@purpose-green.com>
2024-10-18 19:30:39 +0200

    final refactoring

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -1,26 +1,23 @@
-import {Game} from './game';
+import { Game } from "./game";
 
 export class GameRunner {
-    public static main(): void {
-        const game = new Game();
-        game.add("Chet");
-        game.add("Pat");
-        game.add("Sue");
+  public static main(): void {
+    const game = new Game();
+    game.addPlayer("Chet");
+    game.addPlayer("Pat");
+    game.addPlayer("Sue");
 
-        let notAWinner;
-        Array.from({length: 100}).forEach((_, index) => {
-            game.roll(index % 6 + 1);
-        
-            if (index % 10 == 0) {
-            notAWinner = game.wrongAnswer();
-            } else {
-            notAWinner = game.wasCorrectlyAnswered();
-            }
-        
-        });
-    }
+    let notAWinner;
+    Array.from({ length: 100 }).forEach((_, index) => {
+      game.roll((index % 6) + 1);
+
+      if (index % 10 == 0) {
+        notAWinner = game.wrongAnswer();
+      } else {
+        notAWinner = game.wasCorrectlyAnswered();
+      }
+    });
+  }
 }
 
 GameRunner.main();
-
-  
